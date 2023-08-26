e02715657858100cea90380eaa79eb2f4f4a591f
Mihail Golubin <golm2020@gmail.com>
2023-08-26 09:58:33 +0300

    widgets (s) add to end

--- papermodels/forms.py
+++ papermodels/forms.py
@@ -6,7 +6,7 @@ class AddPostForm(forms.ModelForm):
     class Meta:
         model = PaperModels
         fields = ['title', 'description', 'image', 'upload', 'category']
-        widget = {
+        widgets = {
             'title': forms.TextInput(attrs={'class': 'form-input'}),
             'description': forms.Textarea(attrs={'cols': 50, 'rows': 15}),
         }
